3d423e5d80a852bc56f741013a34ea56f0f2e404
Alejandro Laban <alejandro.laban@gmail.com>
2021-08-16 21:29:36 -0500

    [feat] add exception converter

---
+++ src/main/java/com/alejandrolaban/bookingapi/infrastructure/exception/ExceptionUtil.java
@@ -0,0 +1,42 @@
+package com.alejandrolaban.bookingapi.infrastructure.exception;
+
+import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ClassUtils;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
+public class ExceptionUtil {
+
+    private final Gson gson;
+
+    @SneakyThrows
+    public Throwable getThrowableFromJson(String json, String className) {
+        Class<?> clazz = ClassUtils.forName(className, Thread.currentThread().getContextClassLoader());
+        return (Throwable) gson.fromJson(json, clazz);
+    }
+
+    public String getMessageFromThrowableJson(String json, String className) {
+        var throwable = getThrowableFromJson(json, className);
+        return getMessageFromThrowable(throwable);
+    }
+
+    public String getMessageFromThrowable(Throwable throwable) {
+        String messageDetail = throwable.getMessage();
+        if (throwable instanceof ConstraintViolationException) {
+            messageDetail = ((ConstraintViolationException) throwable).getConstraintViolations()
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.joining(StringUtils.LF));
+        }
+
+        return messageDetail;
+    }
+}
